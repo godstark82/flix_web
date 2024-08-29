8240a0b6b0c4ce80276e62eb74e2ac2de48e5bb9
LEADER <godstark82@gmail.com>
2024-08-29 07:39:28 +0530

    added ads.txt in root

--- lib/features/home/presentation/pages/download.dart
+++ lib/features/home/presentation/pages/download.dart
@@ -1,7 +1,6 @@
 import 'package:flix_web/core/utils/utils.dart';
 import 'package:flix_web/features/home/presentation/widgets/header_dl_button.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DownloadSection extends StatelessWidget {
